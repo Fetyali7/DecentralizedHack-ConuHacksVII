ac1e5a151f1b3ee2e15e04314f43a1f07e2e7f9d
Xavier Guertin <xavierguertin@gmail.com>
2023-01-21 13:37:15 -0500

    we believe in aliens

--- transfer.js
+++ transfer.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 async function main() {
 
     //Grab your Hedera testnet account ID and private key from your .env file
-    const myAccountId = process.env.0.0.49363879;
-    const myPrivateKey = process.env.ba067a1acd31c4f0e16d268fb12136275ea12a3fa8890bc8f6af38cc71afd1bc;
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
     // If we weren't able to grab it, we should throw a new error
     if (!myAccountId || !myPrivateKey) {
