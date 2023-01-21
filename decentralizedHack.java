c21e26375674d2523bc91f5e5282cc2e3b641772
Xavier Guertin <xavierguertin@gmail.com>
2023-01-21 11:59:54 -0500

    main

---
+++ decentralizedHack.java
@@ -0,0 +1,14 @@
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class decentralizedHack {
+
+    public static void main(String[] args) {
+        //Grab your Hedera Testnet account ID and private key
+        AccountId myAccountId = AccountId.fromString(Dotenv.load().get("MY_ACCOUNT_ID"));
+        PrivateKey myPrivateKey = PrivateKey.fromString(Dotenv.load().get("MY_PRIVATE_KEY"));  
+
+    }
+}
