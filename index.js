da52386fc6b375c6a6e4ae13d86fc098e1f1d31c
Kadestery <97750132+Kadestery@users.noreply.github.com>
2023-01-21 12:06:55 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client } = require("@hashgraph/sdk");
+require("dotenv").config();
+
+async function main() {
+
+    //Grab your Hedera testnet account ID and private key from your .env file
+    const myAccountId = process.env.0.0.49363879;
+    const myPrivateKey = process.env.ba067a1acd31c4f0e16d268fb12136275ea12a3fa8890bc8f6af38cc71afd1bc;
+
+    // If we weren't able to grab it, we should throw a new error
+    if (!myAccountId || !myPrivateKey) {
+        throw new Error("Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present");
+    }
+}
+main();
